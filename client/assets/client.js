f60769bd0e85f5b2ce31e60e9c7ae833aa87fbd6
Caio Vaccaro <cavaccaro@H7964.ad.hugeinc.com>
2016-04-23 18:53:59 -0300

    Client.js refactor

--- client/assets/client.js
+++ client/assets/client.js
@@ -1,55 +1,74 @@
-var socket = io(),
-  inputs = document.getElementsByTagName('input'),
-  inputsLength = inputs.length,
-  allInputsEmpty = true,
-  i
-
-socket.on('response', function(response) {
-  var conceptLine, apisLine, frameworksLine, finalLine,
+(function() {
+  var socket = io(),
+    inputs = document.getElementsByTagName('input'),
+    responseElement = document.getElementById('response'),
+    inputsLength = inputs.length,
+    allInputsEmpty = true,
     defaultLine = 'Marque suas dificuldades e verei como posso ajudar...',
-    errorLine = 'Parece que não inventaram ainda isso aí...';
+    errorLine = 'Parece que não inventaram ainda isso aí...',
+    moreThanOneConceptLine = 'Você pode usar um desses <b>conceitos</b>:<br>',
+    oneConceptLine = 'O melhor <b>conceito</b> nesse caso é:<br>',
+    moreThanOneApiLine = 'Você pode usar um desses tipos de <b>API</b>:<br>',
+    oneApiLine = 'O melhor tipo de <b>API</b> nesse caso é:<br>',
+    moreThanOneFrameworkLine = 'Você pode usar um desses tipos de <b>frameworks</b>:<br>',
+    oneFrameworkLine = 'O melhor tipo de <b>framework</b> nesse caso é:<br>',
+    itemsSeparator = ' ou ',
+    linesSeparator = '.<br><br>',
+    i, conceptLine, apisLine, frameworksLine, finalLine;
 
-  if(typeof response.Concepts === 'undefined' && allInputsEmpty) {
-    document.getElementById('response').innerHTML = defaultLine;
-    return false;
-  } else if(typeof response.Concepts === 'undefined' && !allInputsEmpty) {
-    document.getElementById('response').innerHTML = errorLine;
-    return false;
+  init();
+
+  function init() {
+    iterateInputs(function(input) {
+      input.addEventListener('change', syncRequest);
+    });
+
+    socket.on('response', gotResponseFromServer);
   }
 
-  conceptLine = response.Concepts.length > 1 ? "Você pode usar um desses <b>conceitos</b>:<br>" : "O melhor <b>conceito</b> nesse caso é:<br>";
-  apisLine = response.APIs.length > 1 ? "Você pode usar um desses tipos de <b>API</b>:<br>" : "O melhor tipo de <b>API</b> nesse caso é:<br>";
-  frameworksLine = response.Frameworks.length > 1 ? "Você pode usar um desses tipos de <b>frameworks</b>:<br>" : "O melhor tipo de <b>framework</b> nesse caso é:<br>";
-  finalLine = conceptLine + response.Concepts.join(' ou ') + '.<br><br>' + apisLine + response.APIs.join(' ou ') + '.<br><br>' + frameworksLine + response.Frameworks.join(' ou ') + '.<br><br>';
-
-  document.getElementById('response').innerHTML = finalLine;
-});
-
-iterateInputs(function(input) {
-  input.addEventListener('change', syncRequest);
-});
-
-function iterateInputs(callback) {
-  for(i = 0; i < inputsLength; i++) {
-    callback(inputs[i])
-  }
-}
-
-function syncRequest() {
-  var request = {};
-  allInputsEmpty = true;
-
-  iterateInputs(function(input) {
-    if(input.checked) {
-      allInputsEmpty = false;
-      request[input.name] = request[input.name] || [];
-      request[input.name].push(input.value);
+  function gotResponseFromServer(response) {
+    if(noData(response) && (allInputsEmpty || !allInputsEmpty)) {
+      responseElement.innerHTML = allInputsEmpty ? defaultLine : errorLine;
+      return false;
     }
-  });
 
-  makeRequest(request);
-}
+    conceptLine = response.Concepts.length > 1 ? moreThanOneConceptLine : oneConceptLine;
+    apisLine = response.APIs.length > 1 ? moreThanOneApiLine : oneApiLine;
+    frameworksLine = response.Frameworks.length > 1 ? moreThanOneFrameworkLine : oneFrameworkLine;
+    finalLine = conceptLine + response.Concepts.join(itemsSeparator) +
+                  linesSeparator + apisLine + response.APIs.join(itemsSeparator) +
+                  linesSeparator + frameworksLine + response.Frameworks.join(itemsSeparator) +
+                  linesSeparator;
 
-function makeRequest(request) {
-  socket.emit('change', request)
-}
+    responseElement.innerHTML = finalLine;
+  }
+
+  function noData(response) {
+    return typeof response.Concepts === 'undefined';
+  }
+
+  function iterateInputs(callback) {
+    for(i = 0; i < inputsLength; i++) {
+      callback(inputs[i])
+    }
+  }
+
+  function syncRequest() {
+    var request = {};
+    allInputsEmpty = true;
+
+    iterateInputs(function(input) {
+      if(input.checked) {
+        allInputsEmpty = false;
+        request[input.name] = request[input.name] || [];
+        request[input.name].push(input.value);
+      }
+    });
+
+    makeRequest(request);
+  }
+
+  function makeRequest(request) {
+    socket.emit('change', request)
+  }
+})();
